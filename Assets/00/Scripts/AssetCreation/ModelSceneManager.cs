9263baa17fababd1b4724787381f7e938be80070
rajandeepsingh13 <rajandeepsingh13@gmail.com>
2023-01-14 10:21:18 -0500

    Working on pallets

---
+++ Assets/00/Scripts/AssetCreation/ModelSceneManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelSceneManager : MonoBehaviour
+{
+
+    public static int currentColor;
+    public Material[] allMaterials;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Color color = new Color("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectColor(int color)
+    {
+        //Change color of prefab buttons
+
+        switch (color) {
+
+        }
+    }
+}
