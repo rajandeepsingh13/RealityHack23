2f9d4b0ac0d643a6a654a2bdc656d9b45a198e59
Ash Shah <>
2023-01-14 13:33:11 -0500

    - Updated node system scripts folder structure - Added scripts for labeled components

---
+++ Assets/00/Scripts/NodeSystem/Components/LabeledDropdown.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 
+/// </summary>
+public class LabeledDropdown : MonoBehaviour
+{
+    #region Inspector Fields
+    #endregion
+
+
+    #region Public Properties
+    #endregion
+
+
+    #region Event Handlers
+    #endregion
+
+
+    #region Internal Variables
+    #endregion
+
+
+    #region Data Constructs
+    #endregion
+
+
+    #region MonoBehaviour Loop
+    private void Start() { }
+    private void Update() { }
+    #endregion
+
+
+    #region Internal Functions
+    #endregion
+
+
+    #region Public API
+    #endregion
+}
---
+++ Assets/00/Scripts/NodeSystem/Components/LabeledToggle.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /// <summary>
+    /// 
+    /// </summary>
+public class LabeledToggle : MonoBehaviour
+{
+    #region Inspector Fields
+
+    #endregion
+
+
+    #region Public Properties
+
+    #endregion
+
+
+    #region Event Handlers
+
+    #endregion
+
+
+    #region Internal Variables
+
+    #endregion
+
+
+    #region Data Constructs
+
+    #endregion
+
+
+    #region MonoBehaviour Loop
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+    #endregion
+    
+
+    #region Internal Functions
+    
+    #endregion
+
+
+    #region Public API
+
+    #endregion
+}
